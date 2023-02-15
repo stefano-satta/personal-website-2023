5f6d759b50459e62d6dac513d1c4621a79824ba6
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2023-02-15 17:49:28 +0100

    about section tech logo implementation; some layout fixes

--- src/components/common/app-icon/AppIcon.tsx
+++ src/components/common/app-icon/AppIcon.tsx
@@ -4,13 +4,14 @@ import {PropsWithChildren} from "react";
 interface AppIconProps {
     url: string | StaticImageData;
     altLabel: string;
+    width?: number;
 }
 
-const AppIcon = ({url, altLabel}: PropsWithChildren<AppIconProps>) => {
+const AppIcon = ({url, altLabel, width = 64}: PropsWithChildren<AppIconProps>) => {
 
     return (
         <div className="app-icon">
-            <Image src={url} alt={altLabel} width={64} priority/>
+            <Image src={url} alt={altLabel} width={width} priority/>
         </div>
     )
 }
--- src/components/feature/section-home/About.tsx
+++ src/components/feature/section-home/About.tsx
@@ -1,38 +1,36 @@
 import {Card, Col, Row} from "react-bootstrap";
 import TitleSection from "@/components/common/title-subtitle-sections/TitleSection";
 import AppIcon from "@/components/common/app-icon/AppIcon";
+import SubtitleSection from "@/components/common/title-subtitle-sections/SubtitleSection";
 import AngularLogo from '../../../../public/logos/angular.png';
 import ReactLogo from '../../../../public/logos/react.png';
 import JavascriptLogo from '../../../../public/logos/javascript.png';
 import TypescriptLogo from '../../../../public/logos/typescript.png';
+import NextLogo from '../../../../public/logos/next.svg';
 
 const About = () => {
     return (
         <section id="about-section-hero" className="container">
             <TitleSection title={'What I Do.'}/>
-            <p className="fs-4 mb-3 text-grey">
-                Experience in developing, testing and
-                debugging web applications and websites
-            </p>
+            <SubtitleSection subtitle={'Experience in developing, testing and debugging web applications and websites'}/>
 
             <Row className="py-3 "style={{height: '29.41176rem'}}>
                 <Col xs="12" md="6">
                     <Card className="card-about h-100">
-                        <div className="fs-3 fw-semibold">
-                            Best of <br/>
-                            Modern technologies
-                        </div>
-
-                        <div className="d-flex">
-                            <AppIcon url={AngularLogo} altLabel={'angular_logo'}/>
-                            <AppIcon url={ReactLogo} altLabel={'react_logo'}/>
+                        <div className="fs-3 fw-semibold">Let's build something together!</div>
+                    </Card>
+                </Col>
+                <Col xs="12" md="6" className="d-flex flex-column justify-content-between">
+                    <Card style={{height: '47%'}} className="d-flex flex-column">
+                        <div className="fs-3 fw-semibold">Modern technologies</div>
+                        <div className="d-flex justify-content-center mt-4">
+                            <AppIcon url={AngularLogo} altLabel={'angular_logo'} width={50}/>
+                            <AppIcon url={ReactLogo} altLabel={'react_logo'} width={50}/>
+                            <AppIcon url={NextLogo} altLabel={'next_logo'} width={60}/>
                             <AppIcon url={JavascriptLogo} altLabel={'js_logo'}/>
                             <AppIcon url={TypescriptLogo} altLabel={'ts_logo'}/>
                         </div>
                     </Card>
-                </Col>
-                <Col xs="12" md="6" className="d-flex flex-column justify-content-between">
-                    <Card style={{height: '47%'}}></Card>
                     <Card style={{height: '47%'}} className="bg-grey">
                         <div className="fs-3 fw-semibold">Projects</div>
                         <div className="fw-ultra-bold position-absolute text-grey projects-count">
