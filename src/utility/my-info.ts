a2da8c62a41ea0cde91dc1916d520e68c31f5d60
stefano <stesatta87@hotmail.it>
2025-01-25 17:55:39 +0100

    reorder position links page

--- src/utility/my-info.ts
+++ src/utility/my-info.ts
@@ -5,7 +5,7 @@ const myInfo: IMyInfo = {
     instagram: 'https://www.instagram.com/_stefanosatta',
     notionCV: 'https://foamy-slicer-832.notion.site/Hello-I-m-Stefano-Satta-fc44f2f38d2f476d860ea70252cd6253',
     sardiniaCoords: 'https://www.google.com/maps/place/Sardegna/@40.0562185,7.8577047,8z/data=!3m1!4b1!4m5!3m4!1s0x12ddc48d448d3591:0x339674b6e4ab6631!8m2!3d40.1208752!4d9.0128926?hl=it',
-    latestUpdate: 'Nov 2024'
+    latestUpdate: 'Jan 2025'
 }
 
 export default myInfo;
